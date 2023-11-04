13e58e4470c5bf29ac7e16f1225b2d341bafb272
Nick Brown <njbrown09@gmail.com>
2023-11-04 17:22:33 -0400

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${TOKEN}" ]; then
+if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${TOKEN}" ] && [ ! -z "${CERT}" ]; then
   echo "Using RTSP stream from $RTSP_URL"
-  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-'AA:BB:CC:00:11:22'}" --cert /client.pem --token "$TOKEN" rtsp -s "$RTSP_URL"
+  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-'AA:BB:CC:00:11:22'}" --cert "$CERT" --token "$TOKEN" rtsp -s "$RTSP_URL"
 fi
 
 exec "$@"
