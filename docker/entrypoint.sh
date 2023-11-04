62465ff0cdf72ac82a0e9e582a07e57580a2d5c6
Nick Brown <njbrown09@gmail.com>
2023-11-04 18:00:10 -0400

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${TOKEN}" ] && [ ! -z "${CERT}" ]; then
-  echo "Using RTSP stream from $RTSP_URL"
-  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-'AA:BB:CC:00:11:22'}" --cert "$CERT" --token "$TOKEN" rtsp -s "$RTSP_URL"
+# Define the path for the certificate
+CERT_PATH="/client.pem"
+
+# Check if client.pem exists at the designated path, if not, create it
+if [ ! -f "${CERT_PATH}" ]; then
+  echo "client.pem not found at ${CERT_PATH}, creating it..."
+  openssl ecparam -out /tmp/private.key -name prime256v1 -genkey -noout
+  openssl req -new -sha256 -key /tmp/private.key -out /tmp/server.csr -subj "/C=TW/L=Taipei/O=Ubiquiti Networks Inc./OU=devint/CN=camera.ubnt.dev/emailAddress=support@ubnt.com"
+  openssl x509 -req -sha256 -days 36500 -in /tmp/server.csr -signkey /tmp/private.key -out /tmp/public.key
+  cat /tmp/private.key /tmp/public.key > "${CERT_PATH}"
+  rm -f /tmp/private.key /tmp/public.key /tmp/server.csr
+else
+  echo "client.pem already exists at ${CERT_PATH}, using existing certificate."
 fi
 
-if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${UNIFI_EMAIL}" ] && [ ! -z "${UNIFI_PASSWORD}" ] && [ ! -z "${CERT}" ]; then
-  echo "Using RTSP stream from $RTSP_URL"
-  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-'AA:BB:CC:00:11:22'}" --cert "$CERT" --nvr-username "$UNIFI_EMAIL" --nvr-password "$UNIFI_PASSWORD" rtsp -s "$RTSP_URL" --model  "UVC G4 Pro"
+# Check if the required variables are set and if TOKEN is set while UNIFI_EMAIL and UNIFI_PASSWORD are not
+if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${TOKEN}" ] && [ -z "${UNIFI_EMAIL}" ] && [ -z "${UNIFI_PASSWORD}" ]; then
+  echo "Using RTSP stream from $RTSP_URL with token authentication"
+  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-AA:BB:CC:00:11:22}" --cert "${CERT_PATH}" --token "$TOKEN" rtsp -s "$RTSP_URL"
+fi
+
+# Check if the required variables are set and if UNIFI_EMAIL and UNIFI_PASSWORD are set while TOKEN is not
+if [ ! -z "${RTSP_URL:-}" ] && [ ! -z "${HOST}" ] && [ ! -z "${UNIFI_EMAIL}" ] && [ ! -z "${UNIFI_PASSWORD}" ] && [ -z "${TOKEN}" ]; then
+  echo "Using RTSP stream from $RTSP_URL with NVR credentials"
+  exec unifi-cam-proxy --host "$HOST" --name "${NAME:-unifi-cam-proxy}" --mac "${MAC:-AA:BB:CC:00:11:22}" --cert "${CERT_PATH}" --nvr-username "$UNIFI_EMAIL" --nvr-password "$UNIFI_PASSWORD" rtsp -s "$RTSP_URL" --model "UVC G4 Pro"
 fi
 
 exec "$@"
